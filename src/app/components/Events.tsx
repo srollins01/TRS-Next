a0bd2600a86dc319eecb76ec9e8510568ca8dfdb
Sarah Rollins <s.rollins751@gmail.com>
2024-09-03 23:28:56 +0900

    Completed scraping function + adjusted card css

--- src/app/components/Events.tsx
+++ src/app/components/Events.tsx
@@ -10,7 +10,7 @@ export default async function Events() {
       <section id="events">
         <h1><strong><em>Upcoming Events</em></strong></h1>
         <div className="col cardspacing">
-          <div className="card">
+          <div className="card inactive-card">
             <h2>No events to display</h2>
           </div>
         </div>
@@ -23,6 +23,9 @@ export default async function Events() {
       <h1><strong><em>Upcoming Events</em></strong></h1>
       <div className="cd-col cardspacing">
         <Card evTitle={events.title} img={events.img} time={events.time} loc={events.loc} desc={events.desc} evLink={events.link} />
+        {events.title2 && <Card evTitle={events.title2} img={events.img2} time={events.time2} loc={events.loc2} desc={events.desc2} evLink={events.link2} />}
+        {events.title3 && <Card evTitle={events.title3} img={events.img3} time={events.time3} loc={events.loc3} desc={events.desc3} evLink={events.link3} />}
+        {events.title4 && <a href="https://www.meetup.com/rpgtokyo/" target="_blank"><div className="card"><h2>See More</h2></div></a>}
       </div>
     </section>
   );
--- src/app/scraper.ts
+++ src/app/scraper.ts
@@ -39,18 +39,177 @@ export default async function Scraper() {
     return Array.from(descgr1).map(p => p?.textContent).join(' ')
   });
 
-  await browser.close();
-
   if (desc1.length > 300) {
     desc1 = desc1.substring(0, 300).trimEnd() + '...'
   }
 
+  try {
+    await page.waitForSelector('#event-card-e-2', {timeout: 5000});
+  } catch (e) {
+    if (e instanceof TimeoutError) {
+      return ({
+        title: evTitle1,
+        title2: null,
+        title3: null,
+        title4: null,
+        img: img1,
+        img2: null,
+        img3: null,
+        time: time1,
+        time2: null,
+        time3: null,
+        loc: loc1,
+        loc2: null,
+        loc3: null,
+        link: link1,
+        link2: null,
+        link3: null,
+        desc: desc1,
+        desc2: null,
+        desc3: null,
+      });
+    }
+  }
+
+  const evTitle2 = await page.evaluate(() => {
+    return document.querySelector('#event-card-e-2 .ds-font-title-3')?.textContent
+  });
+
+  const img2 = await page.evaluate(() => {
+    return document.querySelector('#image-e-2')?.getAttribute('src')
+  });
+
+  const time2 = await page.evaluate(() => {
+    return document.querySelector('#event-card-e-2 time')?.textContent
+  });
+
+  const loc2 = await page.evaluate(() => {
+    return document.querySelector('#event-card-e-2 span.text-gray6')?.textContent
+  });
+
+  const link2 = await page.evaluate(() => {
+    return document.querySelector('#event-card-e-2')?.getAttribute('href')
+  });
+
+  let desc2 = await page.evaluate(() => {
+    const descgr2 = document.querySelectorAll('#event-card-e-2 p')
+    return Array.from(descgr2).map(p => p?.textContent).join(' ')
+  });
+
+  if (desc2.length > 300) {
+    desc2 = desc2.substring(0, 300).trimEnd() + '...'
+  }
+
+  try {
+    await page.waitForSelector('#event-card-e-3', {timeout: 5000});
+  } catch (e) {
+    if (e instanceof TimeoutError) {
+      return ({
+        title: evTitle1,
+        title2: evTitle2,
+        title3: null,
+        title4: null,
+        img: img1,
+        img2: img2,
+        img3: null,
+        time: time1,
+        time2: time2,
+        time3: null,
+        loc: loc1,
+        loc2: loc2,
+        loc3: null,
+        link: link1,
+        link2: link2,
+        link3: null,
+        desc: desc1,
+        desc2: desc2,
+        desc3: null,
+      });
+    }
+  }
+
+  const evTitle3 = await page.evaluate(() => {
+    return document.querySelector('#event-card-e-3 .ds-font-title-3')?.textContent
+  });
+
+  const img3 = await page.evaluate(() => {
+    return document.querySelector('#image-e-3')?.getAttribute('src')
+  });
+
+  const time3 = await page.evaluate(() => {
+    return document.querySelector('#event-card-e-3 time')?.textContent
+  });
+
+  const loc3 = await page.evaluate(() => {
+    return document.querySelector('#event-card-e-3 span.text-gray6')?.textContent
+  });
+
+  const link3 = await page.evaluate(() => {
+    return document.querySelector('#event-card-e-3')?.getAttribute('href')
+  });
+
+  let desc3 = await page.evaluate(() => {
+    const descgr3 = document.querySelectorAll('#event-card-e-3 p')
+    return Array.from(descgr3).map(p => p?.textContent).join(' ')
+  });
+
+  if (desc3.length > 300) {
+    desc3 = desc3.substring(0, 300).trimEnd() + '...'
+  }
+
+  try {
+    await page.waitForSelector('#event-card-e-4', {timeout: 5000});
+  } catch (e) {
+    if (e instanceof TimeoutError) {
+      return ({
+        title: evTitle1,
+        title2: evTitle2,
+        title3: evTitle3,
+        title4: null,
+        img: img1,
+        img2: img2,
+        img3: img3,
+        time: time1,
+        time2: time2,
+        time3: time3,
+        loc: loc1,
+        loc2: loc2,
+        loc3: loc3,
+        link: link1,
+        link2: link2,
+        link3: link3,
+        desc: desc1,
+        desc2: desc2,
+        desc3: desc3,
+      });
+    }
+  }
+
+  const evTitle4 = await page.evaluate(() => {
+    return document.querySelector('#event-card-e-4 .ds-font-title-3')?.textContent
+  });
+
+  await browser.close();
+
   return ({
     title: evTitle1,
+    title2: evTitle2,
+    title3: evTitle3,
+    title4: evTitle4,
     img: img1,
+    img2: img2,
+    img3: img3,
     time: time1,
+    time2: time2,
+    time3: time3,
     loc: loc1,
+    loc2: loc2,
+    loc3: loc3,
     link: link1,
-    desc: desc1
+    link2: link2,
+    link3: link3,
+    desc: desc1,
+    desc2: desc2,
+    desc3: desc3,
   });
 }
