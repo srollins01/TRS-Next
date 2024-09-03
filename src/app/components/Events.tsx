eb121242a0734e7b739ddcaf51569f1f78c550eb
Sarah Rollins <s.rollins751@gmail.com>
2024-09-03 13:48:24 +0900

    Copied css from original React app + added missing Event component html

--- src/app/components/Events.tsx
+++ src/app/components/Events.tsx
@@ -5,9 +5,25 @@ export default async function Events() {
   let events;
   events = await Scraper();
 
+  if (!events) {
+    return (
+      <section id="events">
+        <h1><strong><em>Upcoming Events</em></strong></h1>
+        <div className="col cardspacing">
+          <div className="card">
+            <h2>No events to display</h2>
+          </div>
+        </div>
+      </section>
+    )
+  }
+
   return (
-    <>
-    <Card evTitle={events.title} img={events.img} time={events.time} loc={events.loc} desc={events.desc} evLink={events.link} />
-    </>
+    <section id="events">
+      <h1><strong><em>Upcoming Events</em></strong></h1>
+      <div className="cd-col cardspacing">
+        <Card evTitle={events.title} img={events.img} time={events.time} loc={events.loc} desc={events.desc} evLink={events.link} />
+      </div>
+    </section>
   );
 }
